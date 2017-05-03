e93167158b1154afcc2cf76850d6b7b1392e6bc3
Jaran <jaran1994@gmail.com>
2017-05-03 20:53:08 +0200

    Forsøk på midstilling av hexagons til underside

---
+++ barer2.php
@@ -0,0 +1,51 @@
+<?php
+require_once('db.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<?php include 'header.php'; ?>
+
+<body id="barerimg">
+    <div class="container">
+        <img class="titelundersider" src="http://localhost/something/bilder/kategoribilder/barerlogo.png" width="300px">
+            <div class="undersidor">
+
+            <div class="hexbox2">
+                <a href="restauranter.php">
+                    <div class="hexagon2">
+                        <div class="hexagon-inside">
+                            <div class="hexagonimg" id="restauranter">
+
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="hexbox2">
+                <a href="restauranter.php">
+                    <div class="hexagon2">
+                        <div class="hexagon-inside">
+                            <div class="hexagonimg" id="restauranter">
+
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            </div>
+    </div>
+
+    <!-- Boks for "velkommen" tekst og firmaets  slogan - slutt -->
+
+    <!-- En "pusher" som sørger for ett mellomrom mellom footer og sidenes innhold - start -->
+    <div class="push"></div>
+    <!-- En "pusher" som sørger for ett mellomrom mellom footer og sidenes innhold - slutt -->
+
+    <?php include 'footer.php';?>
+
+</body>
+</html>
